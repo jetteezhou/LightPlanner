8905b1fe0bab178ffb4d1004e6fbe3ee18497b8a
unira-zwj <elonzhou99@gmail.com>
2024-09-25 15:23:39 +0800

    uniplanner

--- uniplanner_llm.py
+++ uniplanner_llm.py
@@ -5,11 +5,9 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# 配置模型路径和设备
 model_name = "/home/chan/zhouweijie/qwen2/checkpoint2-qwen2-1.5B"
 device = "cuda"
 
-# 加载模型和 tokenizer
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     torch_dtype="auto",
@@ -17,15 +15,14 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-# 请求体模型
+
 class ChatRequest(BaseModel):
     model: str
     messages: list
     max_tokens: int = 512
 
-# 生成响应的函数
+
 def generate_response_local(messages, max_tokens=512):
-    # 将消息模板化并进行tokenize
     text = tokenizer.apply_chat_template(
         messages,
         tokenize=False,
@@ -33,23 +30,20 @@ def generate_response_local(messages, max_tokens=512):
     )
     model_inputs = tokenizer([text], return_tensors="pt").to(device)
     
-    # 生成响应
     generated_ids = model.generate(
         **model_inputs,
         max_new_tokens=max_tokens
     )
     
-    # 只取生成的新token部分
     generated_ids = [
         output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
     ]
     
-    # 解码生成的tokens
     response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
     print(response)
     return response
 
-# 定义 API 路由
+
 @app.post("/v1/chat/completions")
 async def chat_completions(request: ChatRequest):
     try:
@@ -57,10 +51,8 @@ async def chat_completions(request: ChatRequest):
         # max_tokens = request.max_tokens
         max_tokens = 512
         
-        # 生成响应
         response_text = generate_response_local(request, max_tokens)
         
-        # 构造返回的JSON格式，模仿OpenAI API
         response = {
             "id": "chatcmpl-123456789",
             "object": "chat.completion",
